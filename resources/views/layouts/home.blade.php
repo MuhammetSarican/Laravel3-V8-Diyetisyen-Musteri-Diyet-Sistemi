95fd1df2d7d6ba1bba40ee0f581b0fad6fe6e76c
MuhammetSarican <muhammetsarican@gmail.com>
2021-11-07 18:58:52 +0300

    template parçalandı

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Kuşbabalı Mahoni">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets')}}/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery-ui.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/aos.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+
+</head>
+<body>
+    @include('home._header')
+    <nav class="site-navigation position-relative text-right" role="navigation">
+    @section('content')
+        İçerik Alanı
+        @show
+
+    </nav>
+    @include('home._footer')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/index', function () {
+    return view('home.index');
+});
+
+Route::get('/home', function () {
+    return view('layouts.home');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
